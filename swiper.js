7ca4b1d91e3f789a8e387434fd6e7bae84be4e6c
LoveRuHunter <113541018+LoveRuHunter@users.noreply.github.com>
2022-10-06 00:21:08 +0500

    update

--- swiper.js
+++ swiper.js
@@ -1,7 +1,29 @@
-let swap = document.querySelector('.swap');
-let Button = document.querySelector('.theme-button')
+let Button = document.querySelector('.swiper-look-more--button') // кнопка скрытия=раскрытия
+let RepairMenuItems = document.querySelectorAll('.swiper-slide__hidden') // коллекция всех элементов с классом swiper-slide__hidden
+let ArrowOfButton = document.querySelector('.swiper-look-more--img') // иконка стрелки
+
 Button.onclick = function() {
-    swap.classList.toggle('Скрыть');
-    swap.classList.toggle('Показать всё');
-    console.log(swap.textContent)
+    let lessText = 'Скрыть' // текст кнопки, когда скрыть
+    let moreText = 'Показать все' // текст кнопки когда показать все
+
+    //  .innerHtml - посмотреть/поменять текст внутри тега
+
+    if (Button.innerHTML === lessText) { // проверка на то, в каком состоянии у нас кнопка в зависимости от текста (Если у кнопки сейчас Скрыть)
+        Button.innerHTML = moreText; // меняем текст кнопки на Показать
+        RepairMenuItems.forEach((element) => {
+            element.style.display = 'none' // к каждому нужному элементу добавляем стиль скрытия
+        })
+        // эквивалент:
+        // for(let i = 0; i <= RepairMenuItems.length; i++) {
+        //     RepairMenuItems[i].style.display = 'none'
+        // }
+        ArrowOfButton.classList.add('swap') // добавляем стрелки класс, который отвечает за ее поворот
+    } else { // если текст у кнопки Показать все
+        Button.innerHTML = lessText; // меняем текст на Скрыть
+        RepairMenuItems.forEach((element) => { // добавляем видимость нужным элементам
+            element.style.display = 'block'
+        })
+        ArrowOfButton.classList.remove('swap') // удаляем поворот у икноки
+    }
+
 };
